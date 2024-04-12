2ab52f9150af816228443b8cd525b5b9664b5464
JaromTMKA <jarom08.cd@gmail.com>
2024-04-12 14:46:12 -0500

    Sesión 2

--- Python/S1/entradaSalida.py
+++ Python/S1/entradaSalida.py
@@ -1,10 +1,3 @@
-# entrada
-#edad = input("dime cuantos años tienes ")# input es la funcion y dentro va el mensaje que quiero mostrar en la consola
-#edad2 = input("dime cuantos años tienes ")
-# el espacio si es un caracter que puedo hacer diferente 2 variables
-#print(edad==edad2)
-#print(edad,edad2)
-# ejemplo 1 : muestra el año de nacimiento de una persona pidiendo su edad por consola ,considerar el año actual 2024
 year_actual=2024
 edad = input("dime cuantos años tienes ")
 edadInt=int(edad)
