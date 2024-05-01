1c476cc73fe3dc0171e6412813e463dc20c2cf1f
JaromTMKA <jarom08.cd@gmail.com>
2024-05-01 14:23:06 -0500

    Opción 1 y 5 Completa y Funcionando

---
+++ Python/Tareas/T3/T3E1/Controller/generarIngresoCSV.py
@@ -0,0 +1,48 @@
+import csv
+from .db import conecta
+
+class generacion():
+    
+    @classmethod
+    def insertar(self):
+        try:
+            with open("Tareas/T3/T3E1/Static/Assets/amazon.csv", 'r', newline='', encoding='utf-8') as CSV:
+                lectorCSV = csv.reader(CSV)
+                nombresColumnas = next(lectorCSV)
+
+            conexion = conecta()
+            cursor = conexion.cursor()
+
+            query = f"CREATE TABLE IF NOT EXISTS Ventas ({', '.join(nombresColumnas)})"
+            cursor.execute(query)
+
+            with open("Tareas/T3/T3E1/Static/Assets/amazon.csv", 'r', newline='', encoding='utf-8') as CSV:
+                next(CSV)  
+                lectorCSV = csv.reader(CSV)
+
+                for fila in lectorCSV:
+                    query2 = f"INSERT INTO Ventas VALUES ({', '.join(['?' for _ in range(len(fila))])})"
+                    cursor.execute(query2, fila)
+
+            conexion.commit()
+            conexion.close()
+
+            print("Generación Exitosa!")
+            return 0
+        except Exception as e:
+            print("Error:", e)
+
+    @classmethod
+    def eliminación(self):
+        try:
+            conexion = conecta()
+            cursor = conexion.cursor()
+
+            query = "DELETE FROM Ventas"
+            cursor.execute(query)
+
+            conexion.commit()
+            conexion.close()
+
+        except Exception as e:
+            print("Error al eliminar registros:", e)
--- Python/Tareas/T3/T3E1/main.py
+++ Python/Tareas/T3/T3E1/main.py
@@ -1,7 +1,10 @@
 from Controller.menu import menu
 from Controller.usuarioController import usuarioController
 from Model.usuario import Usuario
+from Controller.generarIngresoCSV import generacion
+
 import os
+import time
 
 def main():
     os.system("cls")
@@ -15,9 +18,39 @@ def main():
     if (encontrado != None):
         while True:
             os.system("cls")
-
+            
+            print(f"Bienvenid@ {usuario.user_usu}!\n\n")
             menu.mostrar_menu()
             opcion = input("Seleccione una opción: ")
+
+            if opcion == '1':
+                os.system("cls")
+
+                print("Ahora se ejecutará una acción automatizada para el ingreso de un archivo csv a la bd.")
+                time.sleep(3)
+                salidaGen = generacion.insertar()
+
+                if salidaGen == 0:
+                    time.sleep(1.5)
+            #elif opcion == '2':
+                # Opción 2: Generar reporte de ventas
+                #import generar_reporte
+                #generar_reporte.generar_reporte_ventas()
+            #elif opcion == '3':
+                # Opción 3: Generar gráfico de ventas
+                #import generar_grafico
+                #generar_grafico.generar_grafico_ventas()
+            #elif opcion == '4':
+                # Opción 4: Generar bitácora
+                #import bitacora
+                #bitacora.crear_bitacora()
+            elif opcion == '5':
+                generacion.eliminación()
+                print("Saliendo del programa...")
+                break
+            else:
+                print("Opción no válida. Por favor, seleccione una opción válida.")
+
     else :
         os.system("cls")
         print("Al parecer no estás registrado...\nDame los datos que te pediré para registrarte.\n")
@@ -39,4 +72,5 @@ def main():
         else:
             print("Error al registrar usuario. Por favor, inténtalo de nuevo.")
 
-main()
+if __name__ == "__main__":
+    main()
